0d19e914ec645e150bf3886e093f1edd28f9d04d
kostiks <kostiksbusiness@gmail.com>
2024-12-22 18:32:21 +0700

    init commit

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-# List of required packages
-required_packages = ['streamlit', 'matplotlib', 'pandas', 'numpy', 'requests']
-
-# Install missing packages
-for package in required_packages:
-    try:
-        __import__(package)
-    except ImportError:
-        install(package)
-
 import streamlit as st
 import pandas as pd
 import requests
